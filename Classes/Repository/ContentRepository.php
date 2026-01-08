dbdd68a6a465eeb5d6430e02b5d83bd80b45f96b
Georg Ringer <g.ringer@fonda.at>
2026-01-08 13:28:56 +0100

    [TASK] Migrate VersionState::cast

--- Classes/Repository/ContentRepository.php
+++ Classes/Repository/ContentRepository.php
@@ -149,7 +149,7 @@ class ContentRepository
 
         while ($row = $statement->fetchAssociative()) {
             BackendUtility::workspaceOL('tt_content', $row, -99, true);
-            if (is_array($row) && !VersionState::cast($row['t3ver_state'])->equals(VersionState::DELETE_PLACEHOLDER)) {
+            if (is_array($row) && ! VersionState::tryFrom($row['t3ver_state']) === VersionState::DELETE_PLACEHOLDER) {
                 $uid = ($row['_ORIG_uid'] ?? 0) ?: $row['uid'];
                 $rows[$uid] = $uid;
             }
